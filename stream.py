a10cafcb30d4c3262941c6d6f8dfdc1e3dc28199
kmsepr <95290980+kmsepr@users.noreply.github.com>
2025-09-05 11:14:24 +0530

    Update stream.py

--- stream.py
+++ stream.py
@@ -65,8 +65,7 @@ RADIO_STATIONS = {
     "france_24": "https://live.france24.com/hls/live/2037218/F24_EN_HI_HLS/master_500.m3u8",
     "vom_radio": "https://radio.psm.mv/draair",
 
-"mm": "
-"http://surviving-audy-sadiqkm-fb03800f.koyeb.app/tv?station=mazhavil_manorama",
+"mm": "https://surviving-audy-sadiqkm-fb03800f.koyeb.app/tv?station=mazhavil_manorama",
 }
 
 # ── State (single ffmpeg, in-memory recording) ────────────────────────────────
