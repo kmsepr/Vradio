972f3ef388838baae7c336139a4628ef14f32b0a
kmsepr <95290980+kmsepr@users.noreply.github.com>
2025-09-17 08:53:25 +0530

    Update stream.py

--- stream.py
+++ stream.py
@@ -84,7 +84,7 @@ def generate_stream(url):
                 "-i", url,
                 "-vn",
                 "-ac", "1",
-                "-b:a", "32k",
+                "-b:a", "40k",
                 "-f", "mp3",
                 "-"
             ],
